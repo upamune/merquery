6d30bfe86ef342e33521658c5a8060ffa6b35f1f
Yu SERIZAWA <info@serizawa.me>
2017-11-11 23:08:02 +0900

    Add initial query builder

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 📦 Merquery: *UNOFFICIAL* Mercari(jp) item search query builder
 ========================
+
+
+```python
+mq = Merquery()
+url = mq.max_price(1000).min_price(100).keyword('秋本帆華').build()
+print(url)
+
+# Output: https://www.mercari.com/jp/search/?min_price=100&max_price=1000&keyword=%E7%A7%8B%E6%9C%AC%E5%B8%86%E8%8F%AF
+```
+
+## Features
+
+- keyword
+- max price
+- min price
--- merquery/core.py
+++ merquery/core.py
@@ -1,6 +1,66 @@
+from urllib.parse import urlencode
+
 class Merquery:
-	def __init__(self, host:string='', region:string='jp'):
+	__queries={}
+
+	def __init__(self, host: str='https://www.mercari.com', region: str='jp', search_path: str='search'):
 		self.host=host
 		self.region=region
-		self.keyword=''
+		self.search_path=search_path
+		return None
+
+	def keyword(self, keyword: str):
+		self.__queries['keyword']=keyword
+		return self
+
+	def max_price(self, price: int):
+		self.__queries['max_price']=price
+		return self
+
+	def min_price(self, price: int):
+		self.__queries['min_price']=price
+		return self
+
+	def build(self):
+		self.__validate_queries()
+		queries = self.__get_query_str()
+		self.__queries = {}
+		base_url=self.__urljoin(self.host, self.region, self.search_path, '?')
+		return base_url + queries
+
+	def __validate_queries(self):
+		keyword = self.__get_query('keyword')
+		if keyword == None or keyword == "":
+			raise Exception('keyword is empty')
+
+		max_price = self.__get_query('max_price')
+		if max_price:
+			if not isinstance(max_price, int):
+				raise Exception('max_price is not int')
+			if max_price <= 0:
+				raise Exception('max_price should be a  positive number')
+	
+		min_price = self.__get_query('min_price')
+		if min_price:
+			if not isinstance(min_price, int):
+				raise Exception('min_price is not int')
+			if min_price <= 0:
+				raise Exception('min_price should be a  positive number')
+
+		if max_price and min_price and max_price <= min_price:
+			raise Exception('max_price should be greater than min_price')
+	
+
+	def __get_query(self, query: str):
+		try:
+			q = self.__queries[query]
+		except KeyError:
+			return None
+		return q
+
+	def __get_query_str(self):
+		return urlencode(self.__queries)
+
+	def __urljoin(self, *args):
+		return '/'.join(s.strip('/') for s in args)
 
